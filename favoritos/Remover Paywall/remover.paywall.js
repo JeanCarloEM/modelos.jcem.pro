7047a2f87e6652baef3819c59e44f4daa73e2481
Jean Carlo de Elias Moreira <jeancarlo@jeancarloem.com>
2023-08-03 15:16:38 -0300

    Update and rename valor.globo.js to remover.paywall.js
    alterado para swtich case

---
+++ favoritos/Remover Paywall/remover.paywall.js
@@ -0,0 +1,9 @@
+javascript:($=>{
+  switch(window.location.host.trim().toLowerCase*(){
+    /* Valor Globo */
+    case "valor.globo.com":
+      $("main")[0].style += ";overflow: scroll !important;max-height:100vh !important";
+      $("div.barreiraJornada").remove();
+      break;
+  }
+})($)
--- favoritos/Remover Paywall/valor.globo.js
+++
@@ -1,5 +0,0 @@
-// Valor Glogo
-javascript:($=>{
-  $("main")[0].style += ";overflow: scroll !important;max-height:100vh !important";
-  $("div.barreiraJornada").remove();
-})($)
