be0a2acc4c919502c727b2cc547b575ceb37f23d
Jean Carlo de Elias Moreira <jeancarlo@jeancarloem.com>
2023-08-04 14:52:38 -0300

    Update remover.paywall.js

--- favoritos/remover.paywall.js
+++ favoritos/remover.paywall.js
@@ -1,9 +1,15 @@
 javascript:($=>{
-  switch(window.location.host.trim().toLowerCase*(){
+  switch(window.location.host.trim().toLowerCase()){
     /* Valor Globo */
     case "valor.globo.com":
-      $("main")[0].style += ";overflow: scroll !important;max-height:100vh !important";
-      $("div.barreiraJornada").remove();
+      ((x)=>{
+        x();
+        window.setTimeout(x, 500);
+      }(()=>{
+        $("main")[0].style += ";overflow: scroll !important;max-height:100vh !important";
+        $("div.barreiraJornada").remove();
+        $("footer").remove();
+      }))      
       break;
   }
 })($)
