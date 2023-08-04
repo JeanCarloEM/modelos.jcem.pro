dd594c269e5b43f6e1c849013ec778b9a7fe7e43
Jean Carlo de Elias Moreira <jeancarlo@jeancarloem.com>
2023-08-04 15:25:11 -0300

    alterado para leiaisso.net

--- favoritos/remover.paywall.js
+++ favoritos/remover.paywall.js
@@ -1,15 +1,3 @@
-javascript:($=>{
-  switch(window.location.host.trim().toLowerCase()){
-    /* Valor Globo */
-    case "valor.globo.com":
-      ((x)=>{
-        x();
-        window.setTimeout(x, 500);
-      }(()=>{
-        $("main")[0].style += ";overflow: scroll !important;max-height:100vh !important";
-        $("div.barreiraJornada").remove();
-        $("footer").remove();
-      }))      
-      break;
-  }
-})($)
+javascript:(w=>{
+  w.location = "https://leiaisso.net/"+w.location.href;
+})(window)
